20b6f0cdc0865831e1776a92441470efbc5da430
John Beppu <johnb@ziprecruiter.com>
2017-07-30 13:09:09 -0700

    Collect realtime order data from Poloniex
    - Subscribe to every currencyPair on Poloniex
    - orderBookModify and orderBookRemove operations go to the
      poloniex_order table
    - newTrade operations go to the
      poloniex_trade table
    - changed the tags for poloniex_ticker such that
      currencyPair becomes a tag instead of a field since we want that indexed
      for queries.
    
      The exchange tag was removed from poloniex_ticker because
      the name of the table already implies the exchange.
      When we support more exchanges in the future, they should also get
      their own set of tables.

--- index.js
+++ index.js
@@ -3,13 +3,8 @@ const
   autobahn = require('autobahn'),
   moment   = require('moment'),
   Influx   = require('influx'),
-  Koa      = require('koa')
-
-// main
-// ---------
-const
-  cx  = new autobahn.Connection({ url: "wss://api.poloniex.com", realm: "realm1" }),
-  koa = new Koa()
+  Koa      = require('koa'),
+  Poloniex = require('poloniex-api-node')
 
 const influxSchema = {
   host: 'localhost',
@@ -17,35 +12,127 @@ const influxSchema = {
   schema: [
     {
       measurement: 'poloniex_ticker',
-      fields: {
-        'currencyPair': Influx.FieldType.STRING,
-        'last': Influx.FieldType.FLOAT,
-        'lowestAsk': Influx.FieldType.FLOAT,
-        'highestBid': Influx.FieldType.FLOAT,
-        'percentChange': Influx.FieldType.FLOAT,
-        'baseVolume': Influx.FieldType.FLOAT,
-        'quoteVolume': Influx.FieldType.FLOAT,
-        'isFrozen': Influx.FieldType.INTEGER
-      },
       tags: [
-        'exchange'
-      ]
+        'currencyPair'
+      ],
+      fields: {
+        last: Influx.FieldType.FLOAT,
+        lowestAsk: Influx.FieldType.FLOAT,
+        highestBid: Influx.FieldType.FLOAT,
+        percentChange: Influx.FieldType.FLOAT,
+        baseVolume: Influx.FieldType.FLOAT,
+        quoteVolume: Influx.FieldType.FLOAT,
+        isFrozen: Influx.FieldType.INTEGER
+      }
+    },
+    {
+      measurement: 'poloniex_trade',
+      tags: [
+        'currencyPair',
+        'type'
+      ],
+      fields: {
+        amount: Influx.FieldType.FLOAT,
+        date: Influx.FieldType.INTEGER,
+        rate: Influx.FieldType.FLOAT,
+        total: Influx.FieldType.FLOAT,
+        tradeID: Influx.FieldType.INTEGER,
+        type: Influx.FieldType.STRING
+      }
+    },
+    {
+      measurement: 'poloniex_order',
+      tags: [
+        'currencyPair',
+        'type',
+        'op'
+      ],
+      fields: {
+        rate: Influx.FieldType.FLOAT,
+        amount: Influx.FieldType.FLoAT
+      }
     }
   ]
 }
 
-const influx = new Influx.InfluxDB(influxSchema)
+// main
+// ---------
+const
+  cx       = new autobahn.Connection({ url: "wss://api.poloniex.com", realm: "realm1" }),
+  koa      = new Koa(),
+  influx   = new Influx.InfluxDB(influxSchema),
+  poloniex = new Poloniex()
 
 influx.createDatabase('coins')
 
 // cx.onopen = function(s) { s.subscribe('ticker', function(market, ev) { console.log(market) }) };
-cx.onopen = function(s) { s.subscribe('ticker', record) }
+cx.onopen = function(s) {
+  getAllMarkets().then((markets) => {
+    markets.forEach((currencyPair) => {
+      const recordOrders = recordOrdersFn(currencyPair)
+      s.subscribe(currencyPair, recordOrders)
+    })
+  })
+  s.subscribe('ticker', record)
+}
 cx.open()
 console.log('Streaming...')
 
+function getAllMarkets() {
+  return poloniex.returnTicker().then((ticker) => Object.keys(ticker))
+}
+
+function recordOrdersFn(currencyPair) {
+  return function (orders) {
+    console.log(currencyPair, orders);
+    orders.forEach((order) => {
+      const { data } = order
+      switch (order.type) {
+      case 'newTrade':
+        influx.writePoints([
+          {
+            measurement: 'poloniex_trade',
+            tags: {
+              currencyPair: currencyPair,
+              type: data.type
+            },
+            fields: {
+              amount: data.amount,
+              date: moment(data.date).unix(),
+              rate: data.rate,
+              total: data.total,
+              tradeID: data.tradeID
+            }
+          }
+        ])
+        break;
+      case 'orderBookModify':
+      case 'orderBookRemove':
+        influx.writePoints([
+          {
+            measurement: 'poloniex_order',
+            tags: {
+              currencyPair: currencyPair,
+              type: data.type,
+              op: order.type
+            },
+            fields: {
+              amount: data.amount,
+              rate: data.rate
+            }
+          }
+        ])
+        break;
+      default:
+        break;
+      }
+    })
+  }
+}
+
+
 function record(ticker, ev) {
   var f = {
-    currencyPair:  ticker[0],
     last:          parseFloat(ticker[1]),
     lowestAsk:     parseFloat(ticker[2]),
     highestBid:    parseFloat(ticker[3]),
@@ -54,21 +141,12 @@ function record(ticker, ev) {
     quoteVolume:   parseFloat(ticker[6]),
     isFrozen:      ticker[7]
   }
-  console.log(f);
+  console.log(ticker[0], f);
   influx.writePoints([
     {
       measurement: 'poloniex_ticker',
-      tags: { exchange: 'poloniex' },
-      fields: {
-        currencyPair:  ticker[0],
-        last:          parseFloat(ticker[1]),
-        lowestAsk:     parseFloat(ticker[2]),
-        highestBid:    parseFloat(ticker[3]),
-        percentChange: parseFloat(ticker[4]),
-        baseVolume:    parseFloat(ticker[5]),
-        quoteVolume:   parseFloat(ticker[6]),
-        isFrozen:      ticker[7]
-      }
+      tags: { currencyPair: ticker[0] },
+      fields: f
     }
   ]).catch((err) => console.log(err));
 }
