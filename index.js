4136349a9ad66ce9bf524dcc5f2e3d5735210648
John Beppu <johnb@ziprecruiter.com>
2017-08-26 09:19:41 -0700

    Enable bittrex in index.js
    - Disable poloniex for now since the data isn't good enough for analysis.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Koa = require('koa')
 const koa = new Koa()
 
 const poloniex = require('./lib/exchange/poloniex')
-const bittrex = require('./lib/exchange/bittrex')
+const bittrex = require('./lib/exchange/bittrex');
 
-let p = poloniex.ws.init()
-p = poloniex.ws.start(p)
-
-console.log('Streaming...')
+(async function() {
+  let b = await bittrex.rest.init()
+  let b2 = await bittrex.rest.start(b)
+})()
