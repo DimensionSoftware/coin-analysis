aa469628785ea4f56af6fedf7172d659256fd902
John Beppu <johnb@ziprecruiter.com>
2017-08-30 03:31:53 -0700

    Add bin/repl and have it preload useful modules and functions

---
+++ .repl.js
@@ -0,0 +1,19 @@
+function reload(module){
+  delete require.cache[require.resolve(module)]
+  return require(module)
+}
+
+global.reload   = reload
+global.rl       = reload
+global.cl       = console.log
+
+global.Promise  = require('bluebird')
+global.Lazy     = require('lazy.js')
+global.Influx   = require('influx')
+global.moment   = require('moment')
+
+global.helpers  = require('./lib/helpers')
+global.bittrex  = require('./lib/exchange/bittrex')
+global.poloniex = require('./lib/exchange/poloniex')
+global.analysis = require('./lib/analysis')
+global.Bot      = require('./lib/bot')
--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const Koa = require('koa')
 const koa = new Koa()
 
 const poloniex = require('./lib/exchange/poloniex')
-const bittrex = require('./lib/exchange/bittrex');
+const bittrex  = require('./lib/exchange/bittrex')
+
+const token = process.env.TOKEN
 
 (async function() {
-  let b = await bittrex.rest.init()
+  let b  = await bittrex.rest.init()
   let b2 = await bittrex.rest.start(b)
 })()
+
+// [+] laziness
+// [+] Impatience !
+// [+] HUBRIS     !!!
