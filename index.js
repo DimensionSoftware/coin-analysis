3327d28da9a3e9a809aeeda909c4b1e0748e920b
John Beppu <johnb@ziprecruiter.com>
2017-08-31 10:38:49 -0700

    My first crack at anomaly detection
    - It's going to need a lot of tuning.
    - Too many false positives right now.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const koa = new Koa()
 
 const poloniex = require('./lib/exchange/poloniex')
 const bittrex  = require('./lib/exchange/bittrex')
+const Bot      = require('./lib/bot')
 
 const token = process.env.TOKEN
 
 ;(async function() {
-  let b  = await bittrex.rest.init()
-  let b2 = await bittrex.rest.start(b)
+  let b   = await bittrex.rest.init()
+  let bot = new Bot({ events: b.events })
+  let b2  = await bittrex.rest.start(b)
 })()
 
 // [+] laziness
